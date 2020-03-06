6bf79b71061a42818e137f78d7c1d47e75df4311
Rory-Sullivan <rory.sully@hotmail.co.uk>
2020-03-06 12:58:07 +0000

    Created game object and added check for draw.

--- MAIN.js
+++ MAIN.js
@@ -1,36 +1,53 @@
 // Imports
 import { makeMove } from './modules/gameLogic.js';
 
-let winner = false;
-let gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-let playerNo = 1;
+// Create game object to keep track of scores and the game board.
+let game = {
+  board: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  moves: 0,
+  winner: -1, // Value for testing if there is a winner.
+  // -1: no winner, 0: draw, 1: player1 wins, 2: player2 wins.
+  playerTurn: 1, // Player 1 will start.
+  players: 2, // Number of user players (1 or 2).
+  p1Wins: 0,
+  p2Wins: 0,
+  draws: 0,
+  rounds: 0
+};
 
-// Function for when a game button is pressed.
+// Attach game buttons to function.
+let gameButtons = document.querySelectorAll('#buttons button');
+for (let button of gameButtons) {
+  button.onclick = pressed;
+}
+
+// Make a move and check for winner when a game button is pressed.
 function pressed(event) {
   let position = event.target.value;
   event.target.disabled = true;
 
   let symbol;
 
-  if (playerNo == 1) {
+  if (game.playerTurn == 1) {
     symbol = 'X';
   } else {
     symbol = 'O';
   }
   event.target.innerHTML = symbol;
 
-  [winner, playerNo] = makeMove(gameBoard, playerNo, position);
+  makeMove(game, position);
 
-  if (winner) {
+  if (game.winner !== -1) {
     for (let button of gameButtons) {
       button.disabled = true;
     }
-    document.getElementById('winner').innerHTML = `Player ${playerNo} wins.`;
-  }
-}
 
-// Attach game buttons to function.
-let gameButtons = document.querySelectorAll('#buttons button');
-for (let button of gameButtons) {
-  button.onclick = pressed;
+    if (game.winner === 0) {
+      document.getElementById('winner').innerHTML = 'Draw';
+    } else {
+      document.getElementById(
+        'winner'
+      ).innerHTML = `Player ${game.winner} wins.`;
+    }
+  }
 }
--- modules/gameLogic.js
+++ modules/gameLogic.js
@@ -1,66 +1,107 @@
 // Function for making a move and checking if there is a winner.
+//
+// Our game object is below for reference.
+// let game = {
+//   board: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   moves: 0,
+//   winner: -1, // Value for testing if there is a winner.
+//   // -1: no winner, 0: draw, 1: player1 wins, 2: player2 wins.
+//   playerTurn: 1, // Player 1 will start.
+//   players: 2, // Number of user players (1 or 2).
+//   p1Wins: 0,
+//   p2Wins: 0,
+//   draws: 0,
+//   rounds: 0
+// };
 
-function makeMove(gameBoard, playerNo, position) {
+function makeMove(game, position) {
   // Make the move.
-  gameBoard[position] = playerNo;
+  game.board[position] = game.playerTurn;
 
-  // Function for checking if three values are equal.
-  function checkThree() {
-    if (check1 == check2 && check1 == check3 && check1 == playerNo) {
+  // Iterate move count.
+  // Do this before checking for a winner so that we can check for a draw.
+  game.moves++;
+
+  // Check if it is a winning move.
+  checkForWinner();
+
+  // If there is a winner update our win counters and return.
+  if (game.winner !== -1) {
+    if (game.winner === 0) {
+      game.draws++;
+    } else {
+      game[`p${game.winner}Wins`]++;
+    }
+    return true;
+  }
+
+  // If there is no winner.
+  if (game.playerTurn == 1) {
+    game.playerTurn = 2;
+  } else {
+    game.playerTurn = 1;
+  }
+  return false;
+
+  // Function for checking if there is a winner.
+  function checkForWinner() {
+    let check1;
+    let check2;
+    let check3;
+
+    // Check rows
+    for (let i = 0; i < 3; i++) {
+      check1 = game.board[i * 3];
+      check2 = game.board[i * 3 + 1];
+      check3 = game.board[i * 3 + 2];
+      if (checkThree(check1, check2, check3)) {
+        game.winner = game.playerTurn;
+        return true;
+      }
+    }
+
+    // Check columns.
+    for (let i = 0; i < 3; i++) {
+      check1 = game.board[i];
+      check2 = game.board[i + 3];
+      check3 = game.board[i + 6];
+      if (checkThree(check1, check2, check3)) {
+        game.winner = game.playerTurn;
+        return true;
+      }
+    }
+
+    // Check Diagonals.
+    check1 = game.board[0];
+    check2 = game.board[4];
+    check3 = game.board[8];
+    if (checkThree(check1, check2, check3)) {
+      game.winner = game.playerTurn;
+      return true;
+    }
+    check1 = game.board[2];
+    check2 = game.board[4];
+    check3 = game.board[6];
+    if (checkThree(check1, check2, check3)) {
+      game.winner = game.playerTurn;
+      return true;
+    }
+
+    // Check if it is a draw.
+    if (game.moves === 9) {
+      game.winner = 0;
       return true;
     }
     return false;
   }
 
-  // Check if it was a winning move.
-  let check1;
-  let check2;
-  let check3;
-
-  // Check rows
-  for (let i = 0; i < 3; i++) {
-    check1 = gameBoard[i * 3];
-    check2 = gameBoard[i * 3 + 1];
-    check3 = gameBoard[i * 3 + 2];
-
-    if (checkThree()) {
-      return [true, playerNo];
+  // Function for checking if three values are equal.
+  function checkThree(check1, check2, check3) {
+    if (check1 == check2 && check1 == check3 && check1 == game.playerTurn) {
+      return true;
     }
+    return false;
   }
-
-  // Check columns.
-  for (let i = 0; i < 3; i++) {
-    check1 = gameBoard[i];
-    check2 = gameBoard[i + 3];
-    check3 = gameBoard[i + 6];
-
-    if (checkThree()) {
-      return [true, playerNo];
-    }
-  }
-
-  // Check Diagonals.
-  check1 = gameBoard[0];
-  check2 = gameBoard[4];
-  check3 = gameBoard[8];
-  if (checkThree()) {
-    return [true, playerNo];
-  }
-
-  check1 = gameBoard[2];
-  check2 = gameBoard[4];
-  check3 = gameBoard[6];
-  if (checkThree()) {
-    return [true, playerNo];
-  }
-
-  // If there is no winner.
-  if (playerNo == 1) {
-    playerNo = 2;
-  } else {
-    playerNo = 1;
-  }
-  return [false, playerNo];
 }
 
 //Exports
