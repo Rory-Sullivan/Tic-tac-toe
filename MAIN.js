a222edf2e2c0f3582dd814668fd7e99a88ebee07
Rory Sullivan <codingrory@gmail.com>
2020-04-09 16:02:11 +0100

    Linting of main file.

--- MAIN.js
+++ MAIN.js
@@ -5,22 +5,69 @@ import { AI2_Move } from './modules/AI2.js';
 import { AI3_Move } from './modules/AI3.js';
 
 const gameButtons = document.querySelectorAll('#board button');
-gameButtons.forEach((button) => {
-  button.onclick = pressed;
-});
-
 const newGameSelector = document.getElementById('gameMode');
-newGameSelector.onchange = newGame;
-
-const resetButton = document.getElementById('reset');
-resetButton.onclick = reset;
-
 const game = new Game();
+const resetButton = document.getElementById('reset');
 
-modeSelector(); // Adds game.mode with default setting.
+function makeAIMove() {
+  let position;
+
+  // Get position for the move.
+  if (game.mode === 1) {
+    position = AI1_Move(game);
+  } else if (game.mode === 2) {
+    position = AI2_Move(game);
+  } else if (game.mode === 3) {
+    position = AI3_Move(game);
+  }
+
+  // Trigger the appropriate button.
+  gameButtons[position].click();
+}
+
+// Start a new round when reset button is pressed.
+function reset() {
+  game.board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  game.moves = 0;
+  game.winner = 0;
+  game.winningPositions = [];
+
+  document.getElementById(
+    'playerStatus'
+  ).innerHTML = `Player ${game.playerTurn}`;
+
+  gameButtons.forEach((button) => {
+    button.innerHTML = null;
+    button.disabled = false;
+    button.style.color = null;
+  });
+
+  // Make an AI move if the computer is starting.
+  if (game.mode !== 0 && game.playerTurn === 2) {
+    makeAIMove();
+  }
+}
 
 function modeSelector() {
-  game.mode = parseInt(newGameSelector.value);
+  game.mode = parseInt(newGameSelector.value, 10);
+}
+
+function newGame() {
+  game.playerTurn = 1;
+  game.p1Wins = 0;
+  game.p2Wins = 0;
+  game.draws = 0;
+  game.rounds = 0;
+
+  // Update the metrics
+  document.getElementById('p1Wins').innerHTML = game.p1Wins;
+  document.getElementById('p2Wins').innerHTML = game.p2Wins;
+  document.getElementById('draws').innerHTML = game.draws;
+  document.getElementById('rounds').innerHTML = game.rounds;
+
+  modeSelector();
+
+  reset();
 }
 
 // Make a move and check for winner when a game button is pressed.
@@ -41,17 +88,17 @@ function pressed(event) {
   makeMove(game, position);
 
   if (game.winner) {
-    for (const button of gameButtons) {
+    gameButtons.forEach((button) => {
       button.disabled = true;
       button.style.color = 'grey';
-    }
-    for (const position of game.winningPositions) {
+    });
+    game.winningPositions.forEach((winPosition) => {
       if (game.winner === 1) {
-        gameButtons[position].style.color = 'lime';
+        gameButtons[winPosition].style.color = 'lime';
       } else if (game.winner === 2) {
-        gameButtons[position].style.color = 'red';
+        gameButtons[winPosition].style.color = 'red';
       }
-    }
+    });
 
     if (game.winner === -1) {
       document.getElementById('playerStatus').innerHTML = 'Draw!';
@@ -81,59 +128,13 @@ function pressed(event) {
   }
 }
 
-function makeAIMove() {
-  let position;
-
-  // Get position for the move.
-  if (game.mode === 1) {
-    position = AI1_Move(game);
-  } else if (game.mode === 2) {
-    position = AI2_Move(game);
-  } else if (game.mode === 3) {
-    position = AI3_Move(game);
-  }
-
-  // Trigger the appropriate button.
-  gameButtons[position].click();
+function setup() {
+  gameButtons.forEach((button) => {
+    button.onclick = pressed;
+  });
+  newGameSelector.onchange = newGame;
+  resetButton.onclick = reset;
+  modeSelector(); // Adds game.mode with default setting.
 }
 
-function newGame() {
-  game.playerTurn = 1;
-  game.p1Wins = 0;
-  game.p2Wins = 0;
-  game.draws = 0;
-  game.rounds = 0;
-
-  // Update the metrics
-  document.getElementById('p1Wins').innerHTML = game.p1Wins;
-  document.getElementById('p2Wins').innerHTML = game.p2Wins;
-  document.getElementById('draws').innerHTML = game.draws;
-  document.getElementById('rounds').innerHTML = game.rounds;
-
-  modeSelector();
-
-  reset();
-}
-
-// Start a new round when reset button is pressed.
-function reset() {
-  game.board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-  game.moves = 0;
-  game.winner = 0;
-  game.winningPositions = [];
-
-  document.getElementById(
-    'playerStatus'
-  ).innerHTML = `Player ${game.playerTurn}`;
-
-  for (const button of gameButtons) {
-    button.innerHTML = null;
-    button.disabled = false;
-    button.style.color = null;
-  }
-
-  // Make an AI move if the computer is starting.
-  if (game.mode !== 0 && game.playerTurn === 2) {
-    makeAIMove();
-  }
-}
+setup();
