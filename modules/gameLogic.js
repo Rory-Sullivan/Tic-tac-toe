4e7d78ad8d79ebeadc3328157a23350488df2a2b
Rory Sullivan <codingrory@gmail.com>
2020-04-09 16:12:39 +0100

    Linted game logic.

--- modules/gameLogic.js
+++ modules/gameLogic.js
@@ -1,13 +1,39 @@
-/**
- * Controls game flow for our Tic-tac-toe game.
- */
+function checkThree(checkValues, game) {
+  if (
+    checkValues[0] === checkValues[1] &&
+    checkValues[0] === checkValues[2] &&
+    checkValues[0] === game.playerTurn
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function checkForWinner(game) {
+  let checkValues;
+
+  game.getAll().forEach((checkObj) => {
+    checkValues = checkObj.values;
+
+    if (checkThree(checkValues, game)) {
+      game.winner = game.playerTurn;
+      game.winningPositions = checkObj.positions;
+      // So that we don't check for a draw.
+    }
+  });
+
+  // Check if it is a draw.
+  if (game.moves === 9) {
+    game.winner = -1;
+  }
+}
 
 function makeMove(game, position) {
   game.board[position] = game.playerTurn;
 
   // Iterate move count.
   // Do this before checking for a winner so that we can check for a draw.
-  game.moves++;
+  game.moves += 1;
 
   checkForWinner(game);
 
@@ -21,43 +47,12 @@ function makeMove(game, position) {
 
   // If there is a winner update our win counters.
   if (game.winner) {
-    game.rounds++;
+    game.rounds += 1;
     if (game.winner === -1) {
-      game.draws++;
+      game.draws += 1;
     } else {
-      game[`p${game.winner}Wins`]++;
+      game[`p${game.winner}Wins`] += 1;
     }
-  }
-}
-
-function checkForWinner(game) {
-  let checkValues;
-
-  // Check rows, columns and diagonals for a winner.
-  for (const checkObj of game.getAll()) {
-    checkValues = checkObj.values;
-
-    if (checkThree(checkValues)) {
-      game.winner = game.playerTurn;
-      game.winningPositions = checkObj.positions;
-      return; // So that we don't check for a draw.
-    }
-  }
-
-  // Check if it is a draw.
-  if (game.moves === 9) {
-    game.winner = -1;
-  }
-
-  function checkThree(checkValues) {
-    if (
-      checkValues[0] === checkValues[1] &&
-      checkValues[0] === checkValues[2] &&
-      checkValues[0] === game.playerTurn
-    ) {
-      return true;
-    }
-    return false;
   }
 }
 
