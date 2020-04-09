ed367db1b548abd4c83d15d53c9ee120894ca3f0
Rory Sullivan <codingrory@gmail.com>
2020-04-09 16:32:03 +0100

    Linting of AI1 and AI2.

--- modules/AI1.js
+++ modules/AI1.js
@@ -1,38 +1,4 @@
 /**
- * Artificial intelligence for the user to play against.
- * AI1 (EASY)
- * This AI simply makes a random move from the possible moves.
- */
-
-/**
- * Move function for AI1 (EASY)
- */
-function AI1_Move(game) {
-  // Get a random move.
-  let position = getRandInt(0, 8);
-  const origPosition = position; // Original position.
-
-  /*
-    Starting from our random position we iterate through the board until we
-    find an available move.
-    */
-  while (game.board[position] !== 0) {
-    position++;
-
-    // Wrap if we reach the end of the board.
-    if (position === 9) {
-      position = 0;
-    }
-
-    // Make sure we don't have an infinite loop.
-    if (position === origPosition) {
-      throw 'No possible move';
-    }
-  }
-  return position;
-}
-
-/**
  * Gets a random integer between the two input values (inclusive).
  */
 function getRandInt(min, max) {
@@ -43,4 +9,32 @@ function getRandInt(min, max) {
   return Math.floor(randNum * (max - min + 1)) + min;
 }
 
-export { AI1_Move };
+/**
+ * Artificial intelligence for the user to play against.
+ * AI1 (EASY)
+ * This AI simply makes a random move from the possible moves.
+ */
+export default function AI1_Move(game) {
+  // Get a random move.
+  let position = getRandInt(0, 8);
+  const origPosition = position; // Original position.
+
+  /*
+    Starting from our random position we iterate through the board until we
+    find an available move.
+    */
+  while (game.board[position] !== 0) {
+    position += 1;
+
+    // Wrap if we reach the end of the board.
+    if (position === 9) {
+      position = 0;
+    }
+
+    // Make sure we don't have an infinite loop.
+    if (position === origPosition) {
+      throw new Error('No possible move');
+    }
+  }
+  return position;
+}
--- modules/AI2.js
+++ modules/AI2.js
@@ -1,15 +1,68 @@
+import AI1_Move from './AI1.js';
+
+/**
+ * Checks a set of values to see if they are nearly complete.
+ *
+ * @returns the relative position of the empty value, if the set is not
+ * nearly complete returns -1.
+ */
+function checkNearComplete(checkValues, player) {
+  if (
+    checkValues[0] === player &&
+    checkValues[1] === player &&
+    checkValues[2] === 0
+  ) {
+    return 2;
+  }
+  if (
+    checkValues[0] === player &&
+    checkValues[1] === 0 &&
+    checkValues[2] === player
+  ) {
+    return 1;
+  }
+  if (
+    checkValues[0] === 0 &&
+    checkValues[1] === player &&
+    checkValues[2] === player
+  ) {
+    return 0;
+  }
+  return -1;
+}
+
+/**
+ * Searches for a row, column or diagonal that is one move away from being
+ * completed, a "near complete".
+ *
+ * @returns The value of the empty position in the "near complete", returns -1
+ * if no "near complete" is found.
+ */
+export function findNearComplete(game, player) {
+  let emptyPosition = -1;
+  let checkValues;
+  let checkPosition;
+
+  game.getAll().forEach((checkObj) => {
+    checkValues = checkObj.values;
+
+    checkPosition = checkNearComplete(checkValues, player);
+
+    if (checkPosition !== -1) {
+      emptyPosition = checkObj.positions[checkPosition];
+    }
+  });
+
+  return emptyPosition;
+}
+
 /**
  * Artificial intelligence for the user to play against.
  * AI2 (MEDIUM)
  * This AI will try to block the user from completing three in a row and try to
  * finish its own three in a row.
  */
-import { AI1_Move } from './AI1.js';
-
-/**
- * Move function for AI2 (MEDIUM)
- */
-function AI2_Move(game) {
+export function AI2_Move(game) {
   let position;
 
   // First check for a winning move.
@@ -28,62 +81,3 @@ function AI2_Move(game) {
   position = AI1_Move(game);
   return position;
 }
-
-/**
- * Searches for a row, column or diagonal that is one move away from being
- * completed, a "near complete".
- *
- * @returns The value of the empty position in the "near complete", returns -1
- * if no "near complete" is found.
- */
-function findNearComplete(game, player) {
-  let emptyPosition = -1;
-  let checkValues;
-  let checkPosition;
-
-  // Check rows, columns and diagonals.
-  for (const checkObj of game.getAll()) {
-    checkValues = checkObj.values;
-
-    checkPosition = checkNearComplete(checkValues);
-
-    if (checkPosition !== -1) {
-      emptyPosition = checkObj.positions[checkPosition];
-    }
-  }
-
-  return emptyPosition;
-
-  /**
-   * Checks a set of values to see if they are nearly complete.
-   *
-   * @returns the relative position of the empty value, if the set is not
-   * nearly complete returns -1.
-   */
-  function checkNearComplete(checkValues) {
-    if (
-      checkValues[0] == player &&
-      checkValues[1] == player &&
-      checkValues[2] == 0
-    ) {
-      return 2;
-    }
-    if (
-      checkValues[0] == player &&
-      checkValues[1] == 0 &&
-      checkValues[2] == player
-    ) {
-      return 1;
-    }
-    if (
-      checkValues[0] == 0 &&
-      checkValues[1] == player &&
-      checkValues[2] == player
-    ) {
-      return 0;
-    }
-    return -1;
-  }
-}
-
-export { AI2_Move, findNearComplete };
